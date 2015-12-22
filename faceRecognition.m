c5eed2269a34ced5620cd5d9f42a916d8daac5f6
1olf4nL87D2UU <meles.massimiliano@live.it>
2015-12-22 19:29:52 +0100

    Commit Progetto Funzionante
    Ho copiato la funzione extractLBPFeatures presente in Matlab 2015b, e
    riproposta (sono da valutare modifiche), ho creato una bozza di parser
    xml, e abbozzato qualche test sulle distanze tra i vettori di feature
    lbp che sembrano rispecchiare le differenze tra individui diversi
    (yuppiii)

--- faceRecognition.m
+++ faceRecognition.m
@@ -1,8 +1,8 @@
 clc
 clear
-img=imread('M-001-01.bmp');
-img2=imread('M-001-02.bmp');
-img3=imread('M-002-01.bmp');
+img=imread('M-010-01.bmp');
+img2=imread('M-010-02.bmp');
+img3=imread('M-011-01.bmp');
 
 % Viola-Jones [Per ora lo saltiamo finchè non chiediamo al tutor, è un passaggio inutile date le immagini di input]
 %  detector = vision.CascadeObjectDetector('ClassificationModel', 'FrontalFaceLBP') ;
@@ -12,17 +12,36 @@ img3=imread('M-002-01.bmp');
 %  IFaces = insertObjectAnnotation(img, 'rectangle', bboxes, 'Face');
 %  figure, imshow(IFaces), title('Detected faces');
 
-%Metodo che chiama efficientLBP che probabilmente non ci servirà
-%lbpCall();
+paramaters = parseXML();
 
-%Funzione presente in matlab 2015, ci basta prendere il codice, capirlo e
-%commentarlo e abbiamo finito
-features1 = extractLBPFeatures(img); 
-features2 = extractLBPFeatures(img2); 
-features3 = extractLBPFeatures(img3); 
+%Funzione copiata da matlab 2015b, ci basta prendere il codice, capirlo e
+%commentarlo e abbiamo finito. Prende solo immagini in scala di grigio,
+%possibile modifica
+features1 = LBPFeaturesExtractor(rgb2gray(img), 'CellSize', round(size(rgb2gray(img))./4)); 
+features2 = LBPFeaturesExtractor(rgb2gray(img2),'CellSize', round(size(rgb2gray(img2))./4)); 
+features3 = LBPFeaturesExtractor(rgb2gray(img3),'CellSize', round(size(rgb2gray(img3))./4)); 
 
-%giusto per provare la distanza, distanza per stesso individuo
-norm(features1-features2)
 
-%giusto per provare la distanza, distanza per individui diversi
-norm(features1-features3)
+%Distanza tra foto
+foto1_1vs1_2=(features1 - features2).^2;
+foto1_1vs2_1=(features1 - features3).^2;
+
+
+fprintf('Media distanza tra Foto 1 e 2 dello stesso individuo= %d\n',mean (foto1_1vs1_2));
+fprintf('Media distanza tra le Foto 1 di diversi individui= %d\n',mean (foto1_1vs2_1));
+
+figure
+bar([foto1_1vs1_2]', 5, 'grouped')
+title('Squared error of LBP Histograms')
+xlabel('LBP Histogram Bins')
+legend('Foto 1 e 2 Individuo 1')
+
+
+figure
+bar([foto1_1vs2_1 ]', 5,  'grouped')
+title('Squared error of LBP Histograms')
+xlabel('LBP Histogram Bins')
+legend('Foto 1 di diversi individui')
+
+%'I valori del primo grafico sono in generale più bassi di quelli del
+%secondo, infatti la media è inferiore..Ciò vuol dire che c'è meno differenza 
