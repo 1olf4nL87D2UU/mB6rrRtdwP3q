53c026db3a4d2b507924e473e47307e10d2f5d14
1olf4nL87D2UU <meles.massimiliano@live.it>
2015-12-22 13:17:46 +0100

    Test Funzionamento

--- faceRecognition.m
+++ faceRecognition.m
@@ -1,82 +1,28 @@
 clc
 clear
-img=imread('visionteam.jpg');
-imshow('visionteam.jpg')
-%img=rgb2gray(img);
+img=imread('M-001-01.bmp');
+img2=imread('M-001-02.bmp');
+img3=imread('M-002-01.bmp');
 
-% Viola-Jones
-detector = vision.CascadeObjectDetector('ClassificationModel', 'FrontalFaceLBP') ;
+% Viola-Jones [Per ora lo saltiamo finchè non chiediamo al tutor, è un passaggio inutile date le immagini di input]
+%  detector = vision.CascadeObjectDetector('ClassificationModel', 'FrontalFaceLBP') ;
+%  
+%  bboxes = step(detector,img);
+%  
+%  IFaces = insertObjectAnnotation(img, 'rectangle', bboxes, 'Face');
+%  figure, imshow(IFaces), title('Detected faces');
 
-bboxes = step(detector,img);
+%Metodo che chiama efficientLBP che probabilmente non ci servirà
+%lbpCall();
 
-IFaces = insertObjectAnnotation(img, 'rectangle', bboxes, 'Face');
-figure, imshow(IFaces), title('Detected faces');
+%Funzione presente in matlab 2015, ci basta prendere il codice, capirlo e
+%commentarlo e abbiamo finito
+features1 = extractLBPFeatures(img); 
+features2 = extractLBPFeatures(img2); 
+features3 = extractLBPFeatures(img3); 
 
+%giusto per provare la distanza, distanza per stesso individuo
+norm(features1-features2)
 
-% LBP
-
-%% Set demo parameters: tets image and filter parameters
-imgRGB=img;
-
-nFiltSize=8;
-nFiltRadius=1;
-filtR=generateRadialFilterLBP(nFiltSize, nFiltRadius);
-
-fprintf('Here is our filter:\n')
-disp(filtR);
-
-%% Test regular LBP vs RI-LBP
-effLBP= efficientLBP(img, 'filtR', filtR, 'isRotInv', false, 'isChanWiseRot', false);
-effRILBP= efficientLBP(img, 'filtR', filtR, 'isRotInv', true, 'isChanWiseRot', false);
-
-uniqueRotInvLBP=findUniqValsRILBP(nFiltSize);
-tightValsRILBP=1:length(uniqueRotInvLBP);
-% Use this function with caution- it is relevant only if 'isChanWiseRot' is false, or the
-% input image is single-color/grayscale
-effTightRILBP=tightHistImg(effRILBP, 'inMap', uniqueRotInvLBP, 'outMap', tightValsRILBP);
-
-binsRange=(1:2^nFiltSize)-1;
-figure;
-subplot(2,1,1)
-hist(single( effLBP(:) ), binsRange);
-axis tight;
-title('Regular LBP hsitogram', 'fontSize', 16);
-
-subplot(2,2,3)
-hist(single( effRILBP(:) ), binsRange);
-axis tight;
-title('RI-LBP sparse hsitogram', 'fontSize', 16);
-
-subplot(2,2,4)
-hist(single( effTightRILBP(:) ), tightValsRILBP);
-axis tight;
-title('RI-LBP tight hsitogram', 'fontSize', 16);
-
-
-%% Verify 'efficientLBP' and 'pixelwiseLBP' act alike, 
-%% just with different run time and memory utilization
-tic;
- % note this filter dimentions aren't legete...
-effLBP= efficientLBP(imgRGB, 'filtR', filtR, 'isRotInv', true, 'isChanWiseRot', false);
-effTime=toc;
-
-% verify pixel wise implementation returns same results
-tic;
-% same parameters as before
-pwLBP=pixelwiseLBP(imgRGB, 'filtR', filtR, 'isRotInv', true, 'isChanWiseRot', false); 
-inEffTime=toc;
-fprintf('\nRun time ratio %.2f. Same result equality chesk: %o.\n', inEffTime/effTime,...
-   isequal(effLBP, pwLBP));
-
-figure;
-subplot(1, 3, 1)
-imshow(imgRGB);
-title('Original image', 'fontSize', 18);
-
-subplot(1, 3, 2)
-imshow( effLBP );
-title('Efficeint LBP image', 'fontSize', 18);
-
-subplot(1, 3, 3)
-imshow( pwLBP );
-title('Pixel-wise LBP image', 'fontSize', 18);
+%giusto per provare la distanza, distanza per individui diversi
+norm(features1-features3)
